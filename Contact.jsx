d242341097ffd98e238e52c03628175e2e08831a
Sajith Dhananjaya <121706757+sajith230@users.noreply.github.com>
2024-06-29 10:59:24 +0530

    Add files via upload

---
+++ Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+export default function Contact() {
+  return (
+    <div>
+
+<Navbar></Navbar>
+
+
+    </div>
+  )
+}
