2fdcce4600d0d1919d63b991c7bebe274c6f3026
Sajith Dhananjaya <121706757+sajith230@users.noreply.github.com>
2024-06-29 10:56:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/Home/Home';
+
+
+function App() {
+  return (
+  
+      
+    <div className="App">
+     <Navbar></Navbar>
+    <Home/>
+
+
+    
+    </div>
+  );
+}
+
+export default App;
